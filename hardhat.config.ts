baab5bc86c3fe4c82a0a1949292b616f8332193c
Mohammad Rahimi <mammad.rsh@gmail.com>
2022-02-02 16:28:44 +0100

    change hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,22 +10,23 @@ import "solidity-coverage";
 dotenv.config();
 
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+const DEPLOYER_PRIVATE_KEY_LOCAL = process.env.DEPLOYER_PRIVATE_KEY_LOCAL;
+const DEPLOYER_PRIVATE_KEY_RINKEBY = process.env.DEPLOYER_PRIVATE_KEY_RINKEBY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY_LOCAL}`]
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY_RINKEBY}`]
     },
     localhost: {
       url: `http://127.0.0.1:8545`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY_LOCAL}`]
     },
   },
   gasReporter: {
